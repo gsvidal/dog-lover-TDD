c58eeb7f07c6d61f40eb8d1f033b14b3800ca0f7
Gonzo <gvidal.industrial@gmail.com>
2022-08-17 16:24:30 -0500

    Create Card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,43 @@
+import './Card.css';
+import outlinedHeart from '../../assets/icons/outlined-heart.svg';
+import filledHeart from '../../assets/icons/filled-heart.svg';
+import { useState } from 'react';
+
+export const Card = (props) => {
+  const { name, phoneNumber, email, image, favorite } = props;
+
+  const [isFavorite, setIsFavorite] = useState(favorite);
+
+  const toggleFavorite = () => {
+    setIsFavorite((isFavorite) => !isFavorite);
+  };
+
+  return (
+    <>
+      <article className="card">
+        <figure className="card__image-container">
+          <img
+            className="card__image"
+            src={image.url}
+            alt={image.alt}
+            width="170"
+            height="130"
+          />
+          <button onClick={toggleFavorite} className="card__favorite-button">
+            <img
+              src={isFavorite ? filledHeart : outlinedHeart}
+              alt={isFavorite ? 'Filled Heart' : 'Outlined Heart'}
+              className="card__favorite-icon"
+              width="14"
+            />
+          </button>
+        </figure>
+        <div className="card__info">
+          <h1 className="card__pet-name">{name}</h1>
+          <p className="card__phone-number">{phoneNumber}</p>
+          <p className="card__email">{email}</p>
+        </div>
+      </article>
+    </>
+  );
+};
